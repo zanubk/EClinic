b766f4a104a9231e5cc80e94ccb4806dde64e3e5
Khan,Zanub Khan Z <z.khan@student.fontys.nl>
2017-11-07 17:23:19 +0100

    Zanub

---
+++ EClinic-WebApp/EClinic-WebApp/Main_Patient.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Main_Patient.aspx.cs" Inherits="EClinic_WebApp.Main_Patient" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ EClinic-WebApp/EClinic-WebApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EClinic_WebApp.Startup))]
+namespace EClinic_WebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
