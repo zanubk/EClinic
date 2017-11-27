85f7567533aea9267a84100ac0c6d180eb1d9f0f
Khan,Zanub Khan Z <z.khan@student.fontys.nl>
2017-11-27 15:02:19 +0100

    Namespaces Added

--- EClinic-WebApp/EClinic-WebApp/Doctor-Profile.aspx.cs
+++ EClinic-WebApp/EClinic-WebApp/Doctor-Profile.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using EClinic_WebApp.Views;
+using EClinic_WebApp.Presenters;
 
 namespace EClinic_WebApp
 {
